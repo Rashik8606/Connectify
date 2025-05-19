e46514fe4f3bbf7ecf45263466ff2eee87e1744e
rashik <mhdrashik112@gmail.com>
2025-05-19 17:41:35 +0530

    Set Issue

--- connectify/settings.py
+++ connectify/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'connectify'),
         'USER': os.getenv('DB_USER', 'root'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'Rashik@123'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'HOST': os.getenv('DB_HOST','127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
