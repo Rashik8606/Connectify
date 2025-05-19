d659ff0e73f2f11ba831f0086746f53b20180721
rashik <mhdrashik112@gmail.com>
2025-05-19 20:24:47 +0530

    deploy test 1

--- connectify/settings.py
+++ connectify/settings.py
@@ -84,7 +84,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
